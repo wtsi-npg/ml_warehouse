e7520d35fdec73f68dc8719f16899adf018303c3
Marina Gourtovaia <mg8@sanger.ac.uk>
2015-02-03 18:14:04 +0000

    added an option to specify earliest run status an dsome extra fields for the results (needed for aggr. genotype check)

--- lib/WTSI/DNAP/Warehouse/Schema/Query/LibraryDigest.pm
+++ lib/WTSI/DNAP/Warehouse/Schema/Query/LibraryDigest.pm
@@ -4,7 +4,7 @@ use Moose;
 use Carp;
 use Readonly;
 use MooseX::StrictConstructor;
-use List::MoreUtils qw/none/;
+use List::MoreUtils qw/ none /;
 
 our $VERSION = '0';
 
@@ -46,6 +46,20 @@ sub _validate_filter {
   return 1;
 }
 
+=head2 accept_undefined
+
+If the filter attribute is set, undefined is accepted as a valid
+value for the filter. Defaults to false, ie only true values are
+accepted.
+
+=cut
+
+has 'accept_undefined'  => ( isa        => 'Bool',
+                             is         => 'ro',
+                             required   => 0,
+                             default    => 0,
+);
+
 =head2 include_rad
 
 Boolen flag, false by default, ie R&D libraries are not included.
@@ -70,7 +84,8 @@ has 'include_control' => ( isa           => 'Bool',
 
 =head2 completed_after
 
-The earliest qc complete date as DateTime object - an optional attribute.
+The earliest qc complete date as DateTime object or
+the earliest date for required status, an optional attribute.
 
 =cut
 has 'completed_after' => ( isa           => 'Maybe[DateTime]',
@@ -80,7 +95,8 @@ has 'completed_after' => ( isa           => 'Maybe[DateTime]',
 
 =head2 completed_within
 
-Qc complete date range as an array ref of DateTime objects - an optional attribute.
+Qc complete date range as an array ref of DateTime objects or
+a valid time interval for required status, an optional attribute.
 
 =cut
 has 'completed_within' => ( isa           => 'ArrayRef[DateTime]',
@@ -109,6 +125,88 @@ sub _validate_rs {
   return 1;
 }
 
+=head2 earliest_run_status
+
+Earliest run status.
+
+=cut
+
+has 'earliest_run_status'  =>  ( isa        => 'Str',
+                                 is         => 'ro',
+                                 required   => 0,
+);
+
+=head2 id_run
+
+An optional array of run ids.
+
+=cut
+
+has 'id_run'               =>  ( isa        => 'ArrayRef[Int]',
+                                 is         => 'ro',
+                                 required   => 1,
+                                 predicate  => '_has_id_run',
+                                 lazy_build => 1,
+);
+sub _build_id_run {
+  my $self = shift;
+
+  if (!$self->earliest_run_status) {
+    croak 'Should have earliest run status';
+  }
+
+  my $time_expression = $self->_time_interval_query();
+  if (!$time_expression) {
+    croak 'Time limits should be defined';
+  }
+
+  my $schema = $self->iseq_product_metrics->result_source->schema;
+  my @statuses = $schema->resultset('IseqRunStatus')->search(
+    {
+      'me.iscurrent' => 1,
+      'date'         => $time_expression,
+      'run_status_dict.description' =>
+        {'-in' => $self->_valid_run_statuses},
+    },
+    { 'join' => 'run_status_dict',}
+  )->all();
+
+  my @runs = map { $_->id_run } @statuses;
+
+  return \@runs;
+}
+
+has '_valid_run_statuses' =>  ( isa        => 'ArrayRef[Str]',
+                                is         => 'ro',
+                                required   => 1,
+                                lazy_build => 1,
+);
+sub _build__valid_run_statuses {
+  my $self = shift;
+
+  if (!$self->earliest_run_status) {
+    return [];
+  }
+
+  my $schema = $self->iseq_product_metrics->result_source->schema;
+  my $temporal_index_row = $schema->resultset('IseqRunStatusDict')->search(
+    {'iscurrent' => 1, 'description' => $self->earliest_run_status})->next;
+  if (!$temporal_index_row) {
+    croak 'Invalid status: ' . $self->earliest_run_status;
+  }
+  my $temporal_index = $temporal_index_row->temporal_index;
+
+  my @statuses = $schema->resultset('IseqRunStatusDict')->search({
+      'iscurrent' => 1,
+      'temporal_index' => {q[>=], $temporal_index},
+      'description'    => {'-not_like' => '%hold'},
+  })->all();
+
+  @statuses = sort map { $_->description } @statuses;
+
+  return \@statuses;
+}
+
 =head2 create
 
 Creates a digest in a form of a hash reference where the results
@@ -125,7 +223,7 @@ sub create {
   my $self = shift;
 
   my $digest = {};
-  my $flowcell_keys = [];;
+  my $flowcell_keys = [];
   $self->_find_libs($digest, $flowcell_keys);
   $self->_expand_libs($digest, $flowcell_keys);
 
@@ -135,7 +233,6 @@ sub create {
 sub _time_interval_query {
   my $self = shift;
 
-  my $where = {};
   my $dtf = $self->iseq_product_metrics->result_source->storage->datetime_parser;
   my $expression;
   if ($self->completed_after) {
@@ -146,28 +243,32 @@ sub _time_interval_query {
     my @between = map { $dtf->format_datetime($_) } @{$self->completed_within};
     $expression = {q[-between], \@between};
   }
-  if ($expression) {
-    $where->{'iseq_run_lane_metric.run_complete'} = $expression;
-  }
-
-  return $where;
+  return $expression;
 }
 
 sub _find_libs {
   my ($self, $digest, $flowcell_keys) = @_;
 
-  my $quality_field = $self->filter ? $QUALITY_FILTERS{$self->filter} : q[];
-  my $rs = $self->_get_product_rs($self->_time_interval_query());
+  my $where = {};
+  my $with_status = 0;
+  if ($self->_has_id_run || $self->earliest_run_status) {
+    $where->{'me.id_run'} = {'-in', $self->id_run};
+    $with_status = 1;
+  } else {
+    my $time_expression = $self->_time_interval_query();
+    if ($time_expression) {
+      $where->{'iseq_run_lane_metric.run_complete'} = $time_expression;
+    }
+  }
+  my $rs = $self->_get_product_rs($where);
 
   while (my $prow = $rs->next()) {
 
     my $fc_row = $prow->iseq_flowcell;
     if ($fc_row) {
 
-      if ($quality_field) {
-        if (!$fc_row->$quality_field) { # if no qc result or failed
-          next;
-        }
+      if (!$self->_accept($fc_row)) {
+        next;
       }
       if (!$self->include_rad && $fc_row->is_r_and_d) {
         next;
@@ -177,7 +278,7 @@ sub _find_libs {
       }
 
       my $entity = _create_entity($fc_row, $flowcell_keys);
-      _add_entity($digest, $prow, $entity);
+      $self->_add_entity($digest, $prow, $entity, $with_status);
     }
   }
 
@@ -200,7 +301,7 @@ sub _expand_libs {
 
     while (my $prow = $rs->next) {
       my $entity = _create_entity($prow->iseq_flowcell);
-      _add_entity($digest, $prow, $entity, $with_status);
+      $self->_add_entity($digest, $prow, $entity, $with_status);
     }
   }
 
@@ -208,7 +309,22 @@ sub _expand_libs {
 }
 
 sub _add_entity {
-  my ($digest, $prow, $entity, $with_status) = @_;
+  my ($self, $digest, $prow, $entity, $with_status) = @_;
+
+  if ($with_status) {
+    my $status = _get_run_status($prow);
+    if (scalar @{$self->_valid_run_statuses}) {
+      if ( none { $_ eq $status } @{$self->_valid_run_statuses} ) {
+        warn "Skipping entry with status $status\n";
+        return;
+      }
+      if (!$self->_accept($prow->iseq_flowcell)) {
+        warn "Skipping failed entry\n";
+        return;
+      }
+    }
+    $entity->{'status'} = $status;
+  }
 
   my ($instrument_model, $flowcell_barcode, $paired_flag) = _get_run_data($prow);
 
@@ -218,14 +334,21 @@ sub _add_entity {
   my $library = $entity->{'library'};
   delete $entity->{'library'};
   $entity->{'rpt_key'} = $prow->can('rpt_key') ? $prow->rpt_key : _get_rpt_key($prow);
-  if ($with_status) {
-    $entity->{'status'} = _get_run_status($prow);
-  }
 
   push @{$digest->{$library}->{$instrument_model}->{$paired_flag}->{'entities'}},
       $entity;
 
   return;
+}
+
+sub _accept {
+  my ($self, $fc_row) = @_;
+  my $quality_field = $self->filter ? $QUALITY_FILTERS{$self->filter} : q[];
+  if ( $quality_field ) {
+    my $value = $fc_row->$quality_field;
+    return $value || ($self->accept_undefined && !defined $value);
+  }
+  return 1;
 }
 
 sub _get_run_data {
@@ -252,11 +375,16 @@ sub _create_entity {
   my ($fc_row, $fc_keys) = @_;
 
   my $entity = {
-    'sample'  => $fc_row->sample->id_sample_lims,
-    'study'   => $fc_row->study->id_study_lims,
-    'library' => $fc_row->id_library_lims,
-    'id_lims' => $fc_row->id_lims,
+    'sample'      => $fc_row->sample_id,
+    'sample_name' => $fc_row->sample_name,
+    'study'       => $fc_row->study_id,
+    'library'     => $fc_row->id_library_lims,
+    'id_lims'     => $fc_row->id_lims,
   };
+  my $ref = $fc_row->sample_reference_genome || $fc_row->study_reference_genome;
+  if ($ref) {
+    $entity->{'reference_genome'} = $ref;
+  }
 
   push @{$fc_keys}, $fc_row->id_iseq_flowcell_tmp;
 
