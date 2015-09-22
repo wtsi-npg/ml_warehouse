f7714cf8b0f2e4f7304141eafc0fa4e7f8f1bbe4
jenniferliddle <jennifer@jsquared.co.uk>
2015-09-22 14:27:09 +0100

    Added new fields to iseq_product_metric and iseq_run_lane_metric tables

--- lib/WTSI/DNAP/Warehouse/Schema/Result/IseqProductMetric.pm
+++ lib/WTSI/DNAP/Warehouse/Schema/Result/IseqProductMetric.pm
@@ -349,6 +349,15 @@ Indexing read length, bp
   extra: {unsigned => 1}
   is_nullable: 1
 
+=head2 chimeric_reads_percent
+
+  data_type: 'float'
+  extra: {unsigned => 1}
+  is_nullable: 1
+  size: [5,2]
+
+mate_mapped_defferent_chr_5 as percentage of all
+
 =cut
 
 __PACKAGE__->add_columns(
@@ -529,6 +538,13 @@ __PACKAGE__->add_columns(
   },
   'verify_bam_id_snp_count',
   { data_type => 'integer', extra => { unsigned => 1 }, is_nullable => 1 },
+  'chimeric_reads_percent',
+  {
+    data_type => 'float',
+    extra => { unsigned => 1 },
+    is_nullable => 1,
+    size => [5, 2],
+  },
 );
 
 =head1 PRIMARY KEY
@@ -581,111 +597,10 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07036 @ 2014-12-02 12:11:18
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:6m1T2SrRa8dEfSN8LxwocQ
+# Created by DBIx::Class::Schema::Loader v0.07036 @ 2015-09-10 10:35:21
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:dH9M0ZWa6yoB5xGbewyD0w
 
-our $VERSION = '0';
 
-=head2 iseq_run_lane_metric
-
-Type: belongs_to
-
-Related object: L<WTSI::DNAP::Warehouse::Schema::Result::IseqRunLaneMetric>
-
-=cut
-
-__PACKAGE__->belongs_to(
-  'iseq_run_lane_metric_right',
-  'WTSI::DNAP::Warehouse::Schema::Result::IseqRunLaneMetric',
-  { id_run => 'id_run', position => 'position' },
-  { is_deferrable => 1, join_type => 'RIGHT', on_delete => 'CASCADE', on_update => 'NO ACTION' },
-);
-
-=head2 iseq_run_status
-
-Type: has_many
-
-Related object: L<WTSI::DNAP::Warehouse::Schema::Result::IseqRunStatus>
-
-=cut
-
-__PACKAGE__->has_many(
-  'iseq_run_status',
-
-  'WTSI::DNAP::Warehouse::Schema::Result::IseqRunStatus',
-  {
-    'foreign.id_run'   => 'self.id_run',
-  },
-  { cascade_copy => 0, cascade_delete => 0 },
-);
-
-##no critic (ProhibitStringyEval ProhibitPostfixControls ProhibitInterpolationOfLiterals)
-with 'npg_qc::autoqc::role::rpt_key' if eval "require npg_qc::autoqc::role::rpt_key";
-##use critic
-
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
-
 1;
-__END__
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-Result class definition in DBIx binding for the multi-lims warehouse database.
-
-=head1 DIAGNOSTICS
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=head1 SUBROUTINES/METHODS
-
-=head1 DEPENDENCIES
-
-=over
-
-=item strict
-
-=item warnings
-
-=item Moose
-
-=item MooseX::NonMoose
-
-=item MooseX::MarkAsMethods
-
-=item DBIx::Class::Core
-
-=item DBIx::Class::InflateColumn::DateTime
-
-=back
-
-=head1 INCOMPATIBILITIES
-
-=head1 BUGS AND LIMITATIONS
-
-=head1 AUTHOR
-
-Marina Gourtovaia E<lt>mg8@sanger.ac.ukE<gt>
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright (C) 2015 Genome Research Limited
-
-This file is part of the ml_warehouse package L<https://github.com/wtsi-npg/ml_warehouse>.
-
-NPG is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-=cut
-
