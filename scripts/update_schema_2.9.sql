96d1d83bf55691b123ba30a208cddfef609934d7
jmtcsngr <jmtc@sanger.ac.uk>
2018-03-05 15:09:43 +0000

    use single statement alter table

--- scripts/update_schema_2.9.sql
+++ scripts/update_schema_2.9.sql
@@ -1,5 +1,4 @@
 -- The remaining index on (id_run, position, tag_index)
 -- will provide effective indexing for the columns
 -- (id_run, position).
-drop index iseq_pm_run_pos_index on iseq_product_metrics;
-drop index iseq_pr_metrics_lm_fk on iseq_product_metrics;
+ALTER TABLE iseq_product_metrics DROP INDEX iseq_pm_run_pos_index, DROP INDEX iseq_pr_metrics_lm_fk;
