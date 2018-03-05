b2bc135d65187a8519ab586513f0132abb09b2af
jmtcsngr <jmtc@sanger.ac.uk>
2018-03-05 13:18:22 +0000

    fix typo

--- scripts/update_schema_2.9.sql
+++ scripts/update_schema_2.9.sql
@@ -1,5 +1,5 @@
 -- The remaining index on (id_run, position, tag_index)
--- will provideeffective indexing for the columns
+-- will provide effective indexing for the columns
 -- (id_run, position).
 drop index iseq_pm_run_pos_index on iseq_product_metrics;
 drop index iseq_pr_metrics_lm_fk on iseq_product_metrics;
