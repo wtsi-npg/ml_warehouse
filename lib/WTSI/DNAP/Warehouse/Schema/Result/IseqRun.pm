e5d0dbbb6ad25802b51bd1b2179cc4d0ce8037d0
Michael Kubiak <mk35@sanger.ac.uk>
2021-03-30 11:00:54 +0100

    Generate IseqRun result class

---
+++ lib/WTSI/DNAP/Warehouse/Schema/Result/IseqRun.pm
@@ -0,0 +1,149 @@
+
+package WTSI::DNAP::Warehouse::Schema::Result::IseqRun;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+WTSI::DNAP::Warehouse::Schema::Result::IseqRun - Table linking iseq_flowcell table to iseq_run_status table
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components('InflateColumn::DateTime');
+
+=head1 TABLE: C<iseq_run>
+
+=cut
+
+__PACKAGE__->table('iseq_run');
+
+=head1 ACCESSORS
+
+=head2 id_run
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+NPG run identifier
+
+=head2 id_flowcell_lims
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 20
+
+LIMS specific flowcell id
+
+=head2 folder_name
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 64
+
+Runfolder name
+
+=cut
+
+__PACKAGE__->add_columns(
+  'id_run',
+  { data_type => 'integer', extra => { unsigned => 1 }, is_nullable => 0 },
+  'id_flowcell_lims',
+  { data_type => 'varchar', is_nullable => 1, size => 20 },
+  'folder_name',
+  { data_type => 'varchar', is_nullable => 1, size => 64 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-03-30 10:23:33
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gdIbb8UoN0OHvFRDN+Xkng
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+our $VERSION = '0';
+
+__PACKAGE__->meta->make_immutable;
+1;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Result class definition in DBIx binding for the multi-lims warehouse database.
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 SUBROUTINES/METHODS
+
+=head1 DEPENDENCIES
+
+=over
+
+=item strict
+
+=item warnings
+
+=item Moose
+
+=item MooseX::NonMoose
+
+=item MooseX::MarkAsMethods
+
+=item DBIx::Class::Core
+
+=item DBIx::Class::InflateColumn::DateTime
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 AUTHOR
+
+Michael Kubiak E<lt>mk35@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2015 Genome Research Limited
+
+This file is part of the ml_warehouse package L<https://github.com/wtsi-npg/ml_warehouse>.
+
+NPG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
