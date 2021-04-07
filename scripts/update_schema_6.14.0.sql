3faa73b48e707497d48fbea29bb037635642eb58
Carol Scott <ces@sanger.ac.uk>
2021-04-07 18:35:45 +0100

    Update

--- scripts/update_schema_6.14.0.sql
+++ scripts/update_schema_6.14.0.sql
@@ -1,4 +1,14 @@
-<<<<<<< HEAD
+DROP TABLE IF EXISTS `iseq_run`;
+CREATE TABLE `iseq_run` (
+  `id_run` int(10) unsigned NOT NULL COMMENT 'NPG run identifier',
+  `id_flowcell_lims` varchar(20) DEFAULT NULL COMMENT 'LIMS specific flowcell id',
+  `folder_name` varchar(64) DEFAULT NULL COMMENT 'Runfolder name',
+  PRIMARY KEY (`id_run`),
+  KEY `iseq_run_id_flowcell_lims` (`id_flowcell_lims`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci\
+  COMMENT 'Table linking run and flowcell identities with the run folder name';
+
+
 alter table pac_bio_run_well_metrics \
   add column hifi_read_bases \
   bigint unsigned DEFAULT NULL COMMENT 'The number of HiFi bases' after short_insert_percent, \
@@ -18,14 +28,3 @@ alter table pac_bio_run_well_metrics \
   int unsigned DEFAULT NULL COMMENT 'The mean length of HiFi reads filtered due to low quality (<Q20)' after hifi_low_quality_num_reads, \
   add column hifi_low_quality_read_quality_median \
   smallint unsigned DEFAULT NULL COMMENT 'The median base quality of HiFi bases filtered due to low quality (<Q20)' after hifi_low_quality_read_length_mean;
-=======
-DROP TABLE IF EXISTS `iseq_run`;
-CREATE TABLE `iseq_run` (
-  `id_run` int(10) unsigned NOT NULL COMMENT 'NPG run identifier',
-  `id_flowcell_lims` varchar(20) DEFAULT NULL COMMENT 'LIMS specific flowcell id',
-  `folder_name` varchar(64) DEFAULT NULL COMMENT 'Runfolder name',
-  PRIMARY KEY (`id_run`),
-  KEY `iseq_run_id_flowcell_lims` (`id_flowcell_lims`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci\
-  COMMENT 'Table linking run and flowcell identities with the run folder name';
->>>>>>> upstream/devel
