be1b6b9cff2a70b85a18e46ab14302d6312ce2c5
Marina Gourtovaia <mg8@sanger.ac.uk>
2021-07-01 17:08:14 +0100

    Add additional columns to the iseq_heron_product_metrics table.
    Capture the pipeline's URL, ivar_md minimum depth value used with
    ivar and metrics for the consensus computed with an alternative
    ivar minimum depth ivar_amd.

--- scripts/update_schema_6.16.0.sql
+++ scripts/update_schema_6.16.0.sql
@@ -9,3 +9,21 @@ alter table pac_bio_run_well_metrics \
   int unsigned DEFAULT NULL COMMENT 'The mean polymerase read length of the control reads' after control_concordance_mean,
   add column local_base_rate \
   float(8,6) unsigned DEFAULT NULL COMMENT 'The average base incorporation rate, excluding polymerase pausing events' after control_read_length_mean;
+
+ALTER TABLE `iseq_heron_product_metrics` \
+  ADD COLUMN `pp_repo_url` VARCHAR(255) DEFAULT NULL \
+    COMMENT 'URL of the VCS repository for this pipeline' \
+    AFTER `pp_version`, \
+  ADD COLUMN `ivar_md` SMALLINT(5) UNSIGNED DEFAULT NULL \
+    COMMENT 'ivar minimum depth used in generating the default consensus' \
+    AFTER `path_root`, \
+  ADD COLUMN `ivar_amd` SMALLINT(5) UNSIGNED DEFAULT NULL \
+    COMMENT 'ivar minimum depth used in generating the additional consensus' \
+    AFTER `longest_no_n_run`, \
+  ADD COLUMN `pct_N_bases_amd` FLOAT DEFAULT NULL \
+    COMMENT 'Percent of N bases in the additional consensus' \
+    AFTER `ivar_amd`, \
+  ADD COLUMN `longest_no_N_run_amd` SMALLINT(5) UNSIGNED DEFAULT NULL \
+    COMMENT 'Longest data stretch without N in the additional consensus' \
+    AFTER `pct_N_bases_amd`;
+
