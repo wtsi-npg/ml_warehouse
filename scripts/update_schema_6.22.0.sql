835769b70592e365139996152645ed82e093a6ac
mksanger <mk35@sanger.ac.uk>
2022-10-03 16:15:47 +0100

    Add id_product to `pac_bio_run_well_metrics` table

--- scripts/update_schema_6.22.0.sql
+++ scripts/update_schema_6.22.0.sql
@@ -3,3 +3,9 @@ ALTER TABLE pac_bio_product_metrics
   ADD `id_pac_bio_product` CHAR(64) DEFAULT NULL COMMENT 'Product id'
   AFTER `id_pac_bio_tmp`,
   ADD UNIQUE KEY `pac_bio_pr_metrics_id_product` (`id_pac_bio_product`);
+
+-- Add product id to PacBio run well metrics table
+ALTER TABLE pac_bio_run_well_metrics
+  ADD `id_pac_bio_product` CHAR(64) DEFAULT NULL COMMENT 'Product id'
+  AFTER `id_pac_bio_rw_metrics_tmp`,
+  ADD UNIQUE KEY `pac_bio_rw_metrics_id_product` (`id_pac_bio_product`);
