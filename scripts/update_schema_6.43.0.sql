cdb985172a9fdaaaa73cd370d4cc996bed39249f
Marina Gourtovaia <mg8@sanger.ac.uk>
2026-01-22 16:32:30 +0000

    Added ultimagen_application_type column to useq_product_metrics

--- scripts/update_schema_6.43.0.sql
+++ scripts/update_schema_6.43.0.sql
@@ -1,3 +1,8 @@
 ALTER TABLE `seq_product_irods_locations`
   MODIFY `seq_platform_name` enum('Illumina','PacBio','ONT','Elembio','Ultimagen') NOT NULL
     COMMENT 'Name of the sequencing platform used to produce raw data';
+
+ALTER TABLE `useq_product_metrics` \
+  ADD COLUMN `ultimagen_application_type` varchar(255) DEFAULT NULL \
+  COMMENT 'Pipeline/tool which was used for on-board data processing' \
+  AFTER `ultimagen_Library_Name`; 
