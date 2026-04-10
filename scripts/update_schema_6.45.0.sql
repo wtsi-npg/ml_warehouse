2c8d987c615c17f46c94a268b2ea71a6b5ee574c
Carol Scott <ces@sanger.ac.uk>
2026-04-10 15:33:38 +0100

    Tweak

--- scripts/update_schema_6.45.0.sql
+++ scripts/update_schema_6.45.0.sql
@@ -4,14 +4,10 @@
 ALTER TABLE `pac_bio_run_well_metrics` \
   ADD COLUMN `is_cell_multi_use` TINYINT(1) NOT NULL DEFAULT '0' \
     COMMENT 'Is the Revio SMRT Cell a multi-use Cell (available since SMRT Link 25.4).' \
-    AFTER `loading_conc`;
-
-ALTER TABLE `pac_bio_run_well_metrics` \
+    AFTER `loading_conc`, \
   ADD COLUMN `cell_use_count` SMALLINT UNSIGNED DEFAULT NULL \
     COMMENT 'The number of times a SMRT Cell has been used (available since SMRT Link 25.4).' \
-    AFTER `is_cell_multi_use`;
-
-ALTER TABLE `pac_bio_run_well_metrics` \
+    AFTER `is_cell_multi_use`, \
   ADD COLUMN `cell_id` VARCHAR(255) DEFAULT NULL \
     COMMENT 'The SMRT Cell unique identifier (available since SMRT Link 25.4).' \
     AFTER `cell_use_count`;
